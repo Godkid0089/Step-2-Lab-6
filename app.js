c9d6a4ba29a8acb65f8551955e5c7e892df7290e
Godkid0089 <143862337+Godkid0089@users.noreply.github.com>
2023-09-10 13:11:35 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+var body = document.getElementsByTagName('body')[0];
+console.log(body);
+var colors = ['brown','yellow','grey','pink','red','aqua','blue','green','orange'];
+var i=-1;
+function color(){
+    i++;
+    body.style.backgroundColor=colors[i];
+    if(i>=colors.length-1){
+        i=-1;
+    }
+}
+var btn = document.getElementsByTagName('button')[0];
+random = Math.floor(Math.random()*9);
+console.log(btn);
+var buttons = ['brown','yellow','grey','pink','red','aqua','blue','green','orange'];
+var i=-1;
+function color(){
+    i++;
+    body.style.backgroundColor=buttons[i];
+    btn.innerText = colors[i];
+    if(i>=buttons.length-1){
+        i=-1;
+    }
+}
